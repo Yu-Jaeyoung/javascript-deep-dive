9ccc6c5481e3600f2458b6dda1d283e366b93fd4
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-07-28 00:06:19 +0900

    Math example

---
+++ ch29/ex29-11.js
@@ -0,0 +1,5 @@
+// 배열 요소 중에서 최대값 취득
+console.log(Math.max.apply(null, [1, 2, 3]));
+
+// ES6 스프레드 문법
+console.log(Math.max(...[1, 2, 3]));
---
+++ ch29/ex29-13.js
@@ -0,0 +1,5 @@
+// 배열 요소 중에서 최소값 취득
+console.log(Math.min.apply(null, [1, 2, 3]));
+
+// ES6 스프레드 문법
+console.log(Math.min(...[1, 2, 3]));
